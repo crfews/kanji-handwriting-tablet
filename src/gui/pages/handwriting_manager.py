f075c9e453045e8b559d909c35d54e3085eae29c
Phillip Graham <pgraham7@vols.utk.edu>
2025-11-07 10:51:45 -0500

    added functionality for stroke processor

--- src/gui/pages/handwriting_manager.py
+++ src/gui/pages/handwriting_manager.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import QMargins
-from PyQt6.QtWidgets import QWidget, QFormLayout, QLineEdit, QTabWidget, QMessageBox
+from PyQt6.QtWidgets import QWidget, QFormLayout, QLineEdit, QTabWidget, QMessageBox, QLabel, QVBoxLayout
 from gui.widgets.writing_widgets import CharacterDrawing
 from recognition import stroke_processor as sp
 
@@ -36,7 +36,38 @@ class AddUpdateCharacterTab(QWidget):
         else:
             msg += "drawing not submitted"
             QMessageBox.warning(self, "Warning", msg)
-        
+            
+
+class TestCharacterTab(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.char_label = QLabel(self)
+        self.char_label.setText('')
+        layout.addWidget(self.char_label)
+
+        self.character_drawing = CharacterDrawing(self)
+        self.character_drawing.setContentsMargins(QMargins(30, 60, 30, 60))
+        self.character_drawing.submitted.connect(self._on_submit)
+        layout.addWidget(self.character_drawing)
+
+
+    def _on_submit(self, strokes: list[list[float]]) -> None:
+        error = False
+        msg = ""
+
+        found_char = sp.search_strokes(strokes)
+
+        if found_char is None:
+            error = True
+            msg = "No similar character found"
+            QMessageBox.warning(self, "Warning", msg)
+        else:
+            self.char_label.setText(found_char)
+
 
 class HandwritingManager(QTabWidget):
 
@@ -46,7 +77,7 @@ class HandwritingManager(QTabWidget):
         super().__init__(parent)
 
         self.tab1 = AddUpdateCharacterTab(self)
-        self.tab2 = QWidget(self)
+        self.tab2 = TestCharacterTab(self)
 
         self.addTab(self.tab1, "Add/Update Character")
-        self.addTab(self.tab2, "Some Widget")
+        self.addTab(self.tab2, "Test Character")
