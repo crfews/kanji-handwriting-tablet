93e18df3f65db91e126699d07e5bc8bcdab4ac8b
ThomasDonahue01 <tdonahu3@vols.utk.edu>
2026-03-22 21:26:29 -0400

    Updates to the home page

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -2,14 +2,14 @@ from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QMainWindow, QStackedWidget
 from gui.pages.handwriting_manager import HandwritingManager
 #from gui.pages.cards_interface import CardsInterface
-from gui.pages.review_scheduler import ReviewScheduler
+#from gui.pages.review_scheduler import ReviewScheduler
 from gui.pages.QNAPage import QNAPage
 from gui.pages.home_page import HomePage
 #from gui.pages.cards_browser import CardBrowserWidget
 from gui.pages.import_page import ImportPage
 from gui.pages.learn_kana_page import LearnKanaWidget
 from gui.pages.learn_kanji_page import LearnKanjiWidget
-from gui.pages.review_scheduler import ReviewScheduler
+#from gui.pages.review_scheduler import ReviewScheduler
 from gui.pages.review_kana_page import ReviewKanaPage
 from gui.pages.review_kanji_page import ReviewKanjiPage
 from gui.pages.learn_phrase_page import LearnPhrasePage
@@ -34,12 +34,28 @@ class MainWindow(QMainWindow):
         # self.home_page.btn_cards.clicked.connect(
         #     lambda: self.stack.setCurrentWidget(self.cards_page)
         # )
-        self.home_page.btn_review.clicked.connect(
-            lambda: self.stack.setCurrentWidget(self.review)
+        self.home_page.btn_review_kana.clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.review_kana_page)
         )
-        self.home_page.btn_qna.clicked.connect(
-            lambda: self.stack.setCurrentWidget(self.qna_page)
+        self.home_page.btn_review_kanji.clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.review_kanji_page)
         )
+        self.home_page.btn_review_phrase.clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.review_phrase_page)
+        )
+        self.home_page.btn_learn_kana.clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.learn_kana_page)
+        )
+        self.home_page.btn_learn_kanji.clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.learn_kanji_page)
+        )
+        self.home_page.btn_learn_phrase.clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.learn_phrase_page)
+        )
+        self.home_page.btn_import.clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.import_page)
+        )
+        
 
 
     def __init__(self):
@@ -59,15 +75,15 @@ class MainWindow(QMainWindow):
         self.home_page = HomePage(self)
         self.handwriting_page = HandwritingManager(self)
  #       self.cards_page = CardsInterface(self)
-        self.review = ReviewScheduler(self)
-        self.qna_page = QNAPage(self)
+        #self.review = ReviewScheduler(self)
+        #self.qna_page = QNAPage(self)
         self.import_page = ImportPage(self)
         self.learn_kana_page = LearnKanaWidget(self)
         self.learn_kanji_page = LearnKanjiWidget(self)
-        self.review_scheduler = ReviewScheduler(self)
+        self.learn_phrase_page = LearnPhrasePage(self)
+        #self.review_scheduler = ReviewScheduler(self)
         self.review_kana_page = ReviewKanaPage(self)
         self.review_kanji_page = ReviewKanjiPage(self)
-        self.learn_phrase_page = LearnPhrasePage(self)
         self.review_phrase_page = ReviewPhrasePage(self)
 
 
@@ -78,14 +94,14 @@ class MainWindow(QMainWindow):
         self.stack.addWidget(self.home_page)
         self.stack.addWidget(self.handwriting_page)
   #      self.stack.addWidget(self.cards_page)
-        self.stack.addWidget(self.review)
-        self.stack.addWidget(self.qna_page)
+        #self.stack.addWidget(self.review)
+        #self.stack.addWidget(self.qna_page)
         self.stack.addWidget(self.learn_kana_page)
         self.stack.addWidget(self.learn_kanji_page)
-        self.stack.addWidget(self.review_scheduler)
+        self.stack.addWidget(self.learn_phrase_page)
+        #self.stack.addWidget(self.review_scheduler)
         self.stack.addWidget(self.review_kana_page)
         self.stack.addWidget(self.review_kanji_page)
-        self.stack.addWidget(self.learn_phrase_page)
         self.stack.addWidget(self.review_phrase_page)
         
         # Start on home
@@ -95,17 +111,17 @@ class MainWindow(QMainWindow):
         self.connect_home_page_buttons()
         
         # Add pages to the menu
-        self.add_page_to_menu('Import', self.import_page)
         self.add_page_to_menu('Home', self.home_page)
+        self.add_page_to_menu('Import', self.import_page)
         self.add_page_to_menu('Handwriting Manager', self.handwriting_page)
    #     self.add_page_to_menu('Cards Interface', self.cards_page)
-        self.add_page_to_menu('Review', self.review)
-        self.add_page_to_menu('QNA', self.qna_page)
+        #self.add_page_to_menu('Review', self.review)
+        #self.add_page_to_menu('QNA', self.qna_page)
         self.add_page_to_menu('Learn Kana', self.learn_kana_page)
         self.add_page_to_menu('Learn Kanji', self.learn_kanji_page)
-        self.add_page_to_menu('Review Scheduler', self.review_scheduler)
+        self.add_page_to_menu('Learn Phrase', self.learn_phrase_page)
+        #self.add_page_to_menu('Review Scheduler', self.review_scheduler)
         self.add_page_to_menu('Review Kana', self.review_kana_page)
         self.add_page_to_menu('Review Kanji', self.review_kanji_page)
-        self.add_page_to_menu('Learn Phrase', self.learn_phrase_page)
         self.add_page_to_menu('Review Phrase', self.review_phrase_page)
     
--- src/gui/pages/home_page.py
+++ src/gui/pages/home_page.py
@@ -33,36 +33,51 @@ class HomePage(QWidget):
         title.setStyleSheet("font-size: 40px; font-weight: bold; margin-bottom: 30px;")
         title.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
-        cards_layout = QHBoxLayout()
+        cards_layout_review = QHBoxLayout()
+        cards_layout_learn = QHBoxLayout()
 
-        # Create the 3 horizontal buttons
-        self.btn_characters = QPushButton("characters")
-        self.btn_words = QPushButton("words")
-        self.btn_phrases = QPushButton("phrases")
+
+        # Create the 3 horizontal buttons for the review pages
+        self.btn_review_kana = QPushButton("Review Kana")
+        self.btn_review_kanji = QPushButton("Review Kanji")
+        self.btn_review_phrase = QPushButton("Review Phrase")
         
-        # Add horizontal buttons to their layout
-        cards_layout.addWidget(self.btn_characters)
-        cards_layout.addWidget(self.btn_words)
-        cards_layout.addWidget(self.btn_phrases)
+        # Add horizontal buttons to their review layout
+        cards_layout_review.addWidget(self.btn_review_kana)
+        cards_layout_review.addWidget(self.btn_review_kanji)
+        cards_layout_review.addWidget(self.btn_review_phrase)
         
         # Create widget to hold horizontal buttons
-        horizontal_buttons_widget = QWidget()
-        horizontal_buttons_widget.setLayout(cards_layout)
+        horizontal_review_buttons_widget = QWidget()
+        horizontal_review_buttons_widget.setLayout(cards_layout_review)
+
+        # Create the 3 horizontal buttons for the learning pages
+        self.btn_learn_kana = QPushButton("Learn Kana")
+        self.btn_learn_kanji = QPushButton("Learn Kanji")
+        self.btn_learn_phrase = QPushButton("Learn Phrase")
+
+        # Add horizontal buttons to their learn layout
+        cards_layout_learn.addWidget(self.btn_learn_kana)
+        cards_layout_learn.addWidget(self.btn_learn_kanji)
+        cards_layout_learn.addWidget(self.btn_learn_phrase)
+
+        # Create widget to hold horizontal buttons
+        horizontal_learn_buttons_widget = QWidget()
+        horizontal_learn_buttons_widget.setLayout(cards_layout_learn)
 
         # Buttons - store as instance variables for connection in MainWindow
         self.btn_handwriting = QPushButton("Handwriting Manager")
-        self.btn_cards = QPushButton("Cards Interface")
         # self.btn_type = QPushButton("Cards Type Manager")
-        self.btn_review = QPushButton("Review")
-        self.btn_qna = QPushButton("QNA")
+        self.btn_import = QPushButton("Import")
+        
 
         layout.addWidget(title)
         layout.addSpacing(20)
-        layout.addWidget(horizontal_buttons_widget)
+        layout.addWidget(horizontal_review_buttons_widget)
+        layout.addWidget(horizontal_learn_buttons_widget)
         layout.addWidget(self.btn_handwriting)
-        layout.addWidget(self.btn_cards)
         # layout.addWidget(self.btn_type)
-        layout.addWidget(self.btn_review)
-        layout.addWidget(self.btn_qna)
+        layout.addWidget(self.btn_import)
+       
 
         self.setLayout(layout)
